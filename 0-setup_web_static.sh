8f53d7be67ceeb464e9bac24b9130bf8761b4dfb
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-02-02 04:01:33 +0200

    first commit

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Prepare web server for deployment
+apt-get -y update
+apt-get -y install nginx
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" >/data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i "61i\ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
+service nginx restart
---
+++ README.md
@@ -0,0 +1,1 @@
+# AirBnB Clone V2
